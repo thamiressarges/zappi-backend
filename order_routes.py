50beec4c1912411e4e085bfa945fb4342a756a53
Thamires Sarges <thamires.sarges.58@gmail.com>
2026-09-23 17:36:11 -0400

    feat: cria endpoint GET para pedidos

--- order_routes.py
+++ order_routes.py
@@ -1,3 +1,7 @@
 from fastapi import APIRouter 
 
 order_router = APIRouter(prefix='/orders', tags=['orders'])
+
+@order_router.get('/')
+async def pedidos():
+    return {'mensagem': 'rota de pedidos'}
